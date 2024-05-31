50b3830fff5ffdf2d6c726a87bcbf0abf6a90037
ahbejarano <ahbejarano@gmail.com>
2024-05-31 18:18:16 +0200

    Update missing files for cutoff issue

--- chapter-13/src/main/java/org/lwjglb/engine/graph/SceneRender.java
+++ chapter-13/src/main/java/org/lwjglb/engine/graph/SceneRender.java
@@ -202,7 +202,7 @@ public class SceneRender {
         }
 
         uniformsMap.setUniform(prefix + ".conedir", coneDirection);
-        uniformsMap.setUniform(prefix + ".conedir", cutoff);
+        uniformsMap.setUniform(prefix + ".cutoff", cutoff);
         updatePointLight(pointLight, prefix + ".pl", viewMatrix);
     }
 }
